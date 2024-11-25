de2bafb57feef993808accf60e724ebd5ad5142d
Ann-Lore <annlore.schoonyans1@gmail.com>
2024-11-25 15:00:02 +0100

    Update main.py

--- tp7_fraction/main.py
+++ tp7_fraction/main.py
@@ -18,9 +18,9 @@ class Fraction:
         RAISE : ValueError si den est egal à 0 ou si den ou si num n'est pas un entier
         """
         if not isinstance(num,int) or not isinstance(den,int):
-            raise TypeError('num and den must be integers')
+            raise TypeError('num et den doit être des entiers')
         if den == 0 :
-            raise ValueError('den must be greater than 0')
+            raise ValueError('den doit être différent de 0')
 
         if den < 0:
             num = -num
@@ -55,7 +55,7 @@ class Fraction:
         """Return a textual representation of the reduced form of the fraction
 
         PRE :
-        POST : retourne un str donnant le resultat sous forme reduite
+        POST : retourne une chaine de caractère donnant le resultat sous forme réduite
         """
         return f"Le resultat est {self._num}/{self._den}"
 
@@ -66,7 +66,7 @@ class Fraction:
         A mixed number is the sum of an integer and a proper fraction
 
         PRE :
-        POST : renvoie un str donnant le resultat en unite + fraction ou unite si pas de reste
+        POST : renvoie une chaine de caractère donnant le resultat en unite + fraction ou unite si pas de reste
         """
         nbr = self._num // self._den #1
         if self._num % self._den != 0:
@@ -82,10 +82,10 @@ class Fraction:
 
          PRE : other est une fraction à additionner
          POST : retourne le resultat de cette somme de fraction
-         RAISE : erreur si other n'est pas une fraction
+         RAISE : TypeError si other n'est pas une fraction
          """
         if not isinstance(other,Fraction):
-            raise TypeError
+            raise TypeError("other n'est pas une fraction")
         n = self.numerator * other.denominator + self.denominator * other.numerator
         d = self.denominator * other.denominator
         return Fraction(n, d)
@@ -95,10 +95,10 @@ class Fraction:
 
          PRE : other est une fraction à soustraire
          POST : retourne le resultat de cette soustraction de fraction
-         RAISE : erreur si other n'est pas une fraction
+         RAISE : TypeError si other n'est pas une fraction
          """
         if not isinstance(other,Fraction):
-            raise TypeError("other is not a Fraction")
+            raise TypeError("other n'est pas une fraction")
         n = self.numerator * other.denominator - self.denominator * other.numerator
         d = self.denominator * other.denominator
         return Fraction(n, d)
@@ -108,10 +108,10 @@ class Fraction:
 
          PRE : other est une fraction à multiplier
          POST : retourne le resultat de cette multiplication de fraction
-         RAISE : erreur si other n'est pas une fraction
+         RAISE : TypeError si other n'est pas une fraction
          """
         if not isinstance(other,Fraction):
-            raise TypeError("other is not a Fraction")
+            raise TypeError("other n'est pas une fraction")
         n = self.numerator * other.numerator
         d = self.denominator * other.denominator
         return Fraction(n, d)
@@ -121,10 +121,10 @@ class Fraction:
 
          PRE : other est une fraction à diviser
          POST : retourne le resultat de cette division de fraction
-         RAISE : erreur si other n'est pas une fraction et si on veut diviser par 0
+         RAISE : TypeError si other n'est pas une fraction et ValueError si on veut diviser par 0
          """
         if not isinstance(other,Fraction):
-            raise TypeError("other is not a Fraction")
+            raise TypeError("other n'est pas une fraction")
         if other.numerator == 0:
             raise ValueError("le num doit être different de 0")
         n = self.numerator * other.denominator
@@ -136,7 +136,7 @@ class Fraction:
 
          PRE : other est un entier à mettre en puissance
          POST : retourne le resultat de cette puissance de fraction
-         RAISE : erreur si other n'est pas une entier
+         RAISE : TypeError si other n'est pas une entier
          """
         if not isinstance(other,int):
             raise TypeError("Exposant doit être un entier.")
@@ -153,10 +153,10 @@ class Fraction:
 
         PRE : other est une fraction
         POST : retourne True si c'est égale sinon False
-        RAISE : erreur si other n'est pas une fraction
+        RAISE : TypeError si other n'est pas une fraction
         """
         if not isinstance(other,Fraction):
-            raise TypeError("other is not a Fraction")
+            raise TypeError("other n'est pas une fraction")
         return self.numerator == other.numerator and self.denominator == other.denominator
 
     def __float__(self):
@@ -211,54 +211,53 @@ class Fraction:
 
         PRE : other est une fraction
         POST :return True si c'est la difference des fractions est unitaire, sinon False
-        RAISE : erreur si other n'est pas une fraction'
+        RAISE : TypeError si other n'est pas une fraction
         """
         if not isinstance(other,Fraction):
-            raise TypeError("other is not a Fraction")
+            raise TypeError("other n'est pas une fraction")
         dif = self - other
         dif._num = abs(dif.numerator)
         return dif.is_unit()
 
-
-if __name__ == '__main__':
-    pass
-    fraction1 = Fraction(10,20)
-    print(f"1) {fraction1}")
-    fraction2 = Fraction(4,3)
-    print(f"2) {fraction2.as_mixed_number()}")
-
-    fraction3 = Fraction(3,3)
-    print(f"3) {fraction3.as_mixed_number()}")
-
-    fraction4 = Fraction(1,2)
-
-    somme = fraction1 + fraction3
-    print(f"4) {somme}")
-    diff = fraction1 - fraction2
-    print(f"5) {diff}")
-    multi = fraction1 * fraction2
-    print(f"6) {multi}")
-    div = fraction1 / fraction2
-    print(f"7) {div}")
-    puis = fraction1 ** -4
-    print(f"8) {puis}")
-    egal = fraction1 == fraction4
-    print(f"9) {egal}")
-    deci = fraction1
-    print(f"10) {float(deci)}")
-    print(f"11) {fraction1.is_zero()}")
-    fraction5 = Fraction(0,5)
-    print(f"12) {fraction5.is_zero()}")
-    f6 = Fraction(8, 4)
-    f7 = Fraction(1, 3)
-    print(f"13) {f6.is_integer()}")
-    print(f"14) {f7.is_integer()}")
-    f8 = Fraction(1, 4)
-    f9 = Fraction(7, 3)
-    print(f"15) {f8.is_proper()}")
-    print(f"16) {f9.is_proper()}")
-    f1 = Fraction(7, 4)
-    f2 = Fraction(9, 12)
-    f3 = Fraction(-1, 3)
-    print(f"17) {f1.is_adjacent_to(f2)}")
-    print(f"18) {f1.is_adjacent_to(f3)}")
+#
+# if __name__ == '__main__':
+#     fraction1 = Fraction(10,20)
+#     print(f"1) {fraction1}")
+#     fraction2 = Fraction(4,3)
+#     print(f"2) {fraction2.as_mixed_number()}")
+#
+#     fraction3 = Fraction(3,3)
+#     print(f"3) {fraction3.as_mixed_number()}")
+#
+#     fraction4 = Fraction(1,2)
+#
+#     somme = fraction1 + fraction3
+#     print(f"4) {somme}")
+#     diff = fraction1 - fraction2
+#     print(f"5) {diff}")
+#     multi = fraction1 * fraction2
+#     print(f"6) {multi}")
+#     div = fraction1 / fraction2
+#     print(f"7) {div}")
+#     puis = fraction1 ** -4
+#     print(f"8) {puis}")
+#     egal = fraction1 == fraction4
+#     print(f"9) {egal}")
+#     deci = fraction1
+#     print(f"10) {float(deci)}")
+#     print(f"11) {fraction1.is_zero()}")
+#     fraction5 = Fraction(0,5)
+#     print(f"12) {fraction5.is_zero()}")
+#     f6 = Fraction(8, 4)
+#     f7 = Fraction(1, 3)
+#     print(f"13) {f6.is_integer()}")
+#     print(f"14) {f7.is_integer()}")
+#     f8 = Fraction(1, 4)
+#     f9 = Fraction(7, 3)
+#     print(f"15) {f8.is_proper()}")
+#     print(f"16) {f9.is_proper()}")
+#     f1 = Fraction(7, 4)
+#     f2 = Fraction(9, 12)
+#     f3 = Fraction(-1, 3)
+#     print(f"17) {f1.is_adjacent_to(f2)}")
+#     print(f"18) {f1.is_adjacent_to(f3)}")
